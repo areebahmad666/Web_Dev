4468824709e4bc229930df4fade38364803c2906
areebahmad666 <areebahmad666@gmail.com>
2025-05-14 15:20:04 +0530

    w

--- day2/abc.js
+++ day2/abc.js
@@ -1,23 +1,3 @@
-// console.log("5"+2+4);
-// console.log(2+3+"4");
-// console.log("5"-1);
-// console.log(1-"2");
-// console.log('5'==5);
-// console.log('5'===5);
-
-// let a=14
-// let b=3
-// c=a+b
-// console.log("The value of "+c);
-// console.log(`the value of ${a}+${b}=${c}`)
-
-// function table(){
-//     let val=document.getElementById("input").value
-//     for(i=1;i<11;i++){
-//         console.log(i*val);
-        
-//     }
-// }
 
 let arr = [60,4,1,11];
 let max =arr[0]
